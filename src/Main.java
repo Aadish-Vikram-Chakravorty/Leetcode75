a53dda869b84f7c2723a2f43f9dbe4630fe972bd
Aadish Vikram Chakravorty <aadishchakravorty@gmail.com>
2024-11-27 00:16:39 +0530

    commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            merged.append(word1.charAt(i++));
+            merged.append(word2.charAt(j++));
+        }
+
+        while (i < word1.length()) {
+            merged.append(word1.charAt(i++));
+        }
+        while (j < word2.length()) {
+            merged.append(word2.charAt(j++));
+        }
+
+        return merged.toString();
+    }
+
+    public static void main(String[] args) {
+        String word1 = "abc";
+        String word2 = "pqr";
+        System.out.println(mergeAlternately(word1, word2));
+    }
+}
